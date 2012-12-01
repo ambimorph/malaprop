972845b61c46cf472f5add06d19c0e5f1e5bd8cb
Amber on abhyasa <amber@abhyasa>
2012-12-01 15:23:29 -0700

    three tests passing

--- code/error_insertion/RealWordErrorChannel.py
+++ code/error_insertion/RealWordErrorChannel.py
@@ -6,12 +6,12 @@ import codecs, unicodedata
 
 class RealWordErrorChannel():
 
-    def __init__(self, segmenter_training_text_infile_obj, vocabfile_obj, outfile_obj, p, random_seed):
+    def __init__(self, segmenter_training_text_infile_obj, vocabfile_obj, outfile_obj, p, random_number_generator):
         self.unicode_vocabfile_obj = codecs.getreader('utf-8')(vocabfile_obj)
         self.real_words, self.symbols = self.get_real_words_and_symbols()
         # Pass infile through NLTKSegmentThenTokenise to train
         self.p = p
-        self.random_seed = random_seed
+        self.random_number_generator = random_number_generator
         self.real_word_errors = 0
         self.real_word_tokens_passed_though = 0
         self.mean_errors_per_word = 0
@@ -44,29 +44,55 @@ class RealWordErrorChannel():
     def is_real_word(self, token):
         return token in self.real_words
 
-    def create_error_with_probability_p(self, left_char, right_char, p=None, random_seed=None):
+    def create_error_with_probability_p(self, left_char, right_char, p=None, random_number_generator=None):
+        assert left_char != u'' or right_char != u''
         if p == None: p = self.p
-        if random_seed == None: random_seed = self.random_seed
-        # return string, error_was_created
-        return u'', False
+        if random_number_generator == None: random_number_generator = self.random_number_generator
 
-    def pass_token_through_channel(self, token, random_seed=None):
+        if random_number_generator.random() < p: # create an error
+            # 0 = Insertion, 1 = Deletion, 2 = Substitution, 3 = Transposition
+
+            if left_char == u'': # No transposition
+                error_type = random_number_generator.randrange(3)
+            elif right_char == u'': # Insertion only
+                error_type = 0
+            else:
+                error_type = random_number_generator.randrange(4)
+            
+            if error_type == 0: # "insertion!"
+                return left_char + random_number_generator.choice(self.symbols) + right_char, True
+            elif error_type == 1: # "deletion!"
+                return left_char, True
+            elif error_type == 2: # "substitution!"
+                return left_char + random_number_generator.choice(self.symbols), True
+            else: # "transposition!"
+                return right_char + left_char, True
+            
+        else:
+            return left_char + right_char, False
+
+    def pass_token_through_channel(self, token, random_number_generator=None):
         # If token is a real word, pass each pair of chars through insert_error, else return original
         # if result is a real word, return it (possibly re-uppering), else return original
         # Update real_word_errors, real_word_tokens_passed_though, mean_errors_per_word, max_errors_per_word
-        if random_seed == None: random_seed = self.random_seed
-        return u''
+        if random_number_generator == None: random_number_generator = self.random_number_generator
+        result = u''
+        end_of_buffer_position = 1
 
-    def pass_sentence_through_channel(self, (original, boundaries, substitutions), random_seed=None):
+        left = 0
+        
+        return result
+
+    def pass_sentence_through_channel(self, (original, boundaries, substitutions), random_number_generator=None):
         # Use boundaries to find tokens
         # If there are no substitutions within the token, pass it through the channel first,
         # else just write it out.
-        if random_seed == None: random_seed = self.random_seed
+        if random_number_generator == None: random_number_generator = self.random_number_generator
 
-    def pass_file_through_channel(self, text=None, random_seed=None):
+    def pass_file_through_channel(self, text=None, random_number_generator=None):
         assert text is None or isinstance(text, unicode), text
         if text == None: text = self.text # change this to say unicode-read training text
-        if random_seed == None: random_seed = self.random_seed
+        if random_number_generator == None: random_number_generator = self.random_number_generator
         # Put through Segmenter_tokeniser
         # For sentence in segmenter_tokeniser output,
         # pass sentence through channel.
--- code/error_insertion/test/test_RealWordErrorChannel.py
+++ code/error_insertion/test/test_RealWordErrorChannel.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # 2012 L. Amber Wilcox-O'Hearn
 # test_RealWordErrorChannel.py
 
@@ -12,56 +13,64 @@ class RealWordErrorChannelTest(unittest.TestCase):
         text_to_corrupt = open('test_data/text_to_corrupt', 'rb')
         vocab_file = open('test_data/1K_test_vocab', 'rb')
         corrupted = open('/tmp/test_error_corpus', 'wb')
-        p = .2
+        p = .3
         self.real_word_error_channel = RealWordErrorChannel.RealWordErrorChannel(text_to_corrupt, vocab_file, corrupted, p, None)
 
     def test_real_words(self):
-        test_word = u'with'
-        assert self.real_word_error_channel.is_real_word(test_word), test_word
-        test_word = u'end.of.document'
-        assert self.real_word_error_channel.is_real_word(test_word), test_word
-        test_word = u'xxxx'
-        assert not self.real_word_error_channel.is_real_word(test_word), test_word
-        test_word = u'-'
-        assert not self.real_word_error_channel.is_real_word(test_word), test_word
-        test_word = u'.'
-        assert not self.real_word_error_channel.is_real_word(test_word), test_word
+        for test_word in [u'with', u'end.of.document', u'don\'t']:
+            assert self.real_word_error_channel.is_real_word(test_word), test_word
+        for test_word in [u'xxxx', u'-', u'.', u'<3-digit-integer>']:
+            assert not self.real_word_error_channel.is_real_word(test_word), test_word
 
     def test_symbols(self):
-        test_symbol = u'x'
-        assert test_symbol in self.real_word_error_channel.symbols
-        test_symbol = u'x'
-        assert test_symbol in self.real_word_error_channel.symbols
-        test_symbol = u'x'
-        assert test_symbol not in self.real_word_error_channel.symbols
+        for test_symbol in [u'x', u'\'']:
+            assert test_symbol in self.real_word_error_channel.symbols, test_symbol
+        for test_symbol in [u'β']:
+            assert test_symbol not in self.real_word_error_channel.symbols, test_symbol
 
     def test_create_error_with_probability_p(self):
         r = random.Random(999)
-        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(None, u'a', 1, random_seed=r)
-        expected_result = u'xxxxx'
+
+        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(u'', u'a', 1, random_number_generator=r)
+        expected_result = u'va'
         assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
-        assert error_was_created == False, u'Expected False, got ' + str(error_was_created)
-        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(None, u'a', .2, random_seed=r)
-        expected_result = u'xxxxx'
-        assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
-        assert error_was_created == False, u'Expected False, got ' + str(error_was_created)
-        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(u's', u'a', 1, random_seed=r)
-        expected_result = u'xxxxx'
-        assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
-        assert error_was_created == False, u'Expected False, got ' + str(error_was_created)
-        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(u's', None, 1, random_seed=r)
-        expected_result = u'xxxxx'
+        assert error_was_created == True, u'Expected True, got ' + str(error_was_created)
+
+        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(u'', u'a', .1, random_number_generator=r)
+        expected_result = u'a'
         assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
         assert error_was_created == False, u'Expected False, got ' + str(error_was_created)
 
+        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(u's', u'a', 1, random_number_generator=r)
+        expected_result = u'sta'
+        assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
+        assert error_was_created == True, u'Expected True, got ' + str(error_was_created)
+
+        result, error_was_created = self.real_word_error_channel.create_error_with_probability_p(u's', u'', 1, random_number_generator=r)
+        expected_result = u'st'
+        assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
+        assert error_was_created == True, u'Expected True, got ' + str(error_was_created)
+
     def test_pass_token_through_channel(self):
+
         r = random.Random(999)
-        result = self.real_word_error_channel.pass_token_through_channel(u'this', random_seed=r)
-        expected_result = u'xxxxx'
-        assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
-        result = self.real_word_error_channel.pass_token_through_channel(u'that', random_seed=r)
-        expected_result = u'xxxxx'
-        assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
+
+        test_cases = [ \
+            (u'a', u'xxx', 100, 100, 100, 100), \
+            (u'and', u'xxx', 100, 100, 100, 100), \
+            (u'"', u'"', 100, 100, 100, 100), \
+                ]
+
+        for test_case in test_cases:
+
+            result = self.real_word_error_channel.pass_token_through_channel(test_case[0], random_number_generator=r)
+            expected_result = test_case[1]
+            assert result == expected_result, u'Expected ' + expected_result + ', but got ' + result
+            assert self.real_word_error_channel.real_word_errors == test_case[2]
+            assert self.real_word_error_channel.real_word_tokens_passed_though == test_case[3]
+            assert self.real_word_error_channel.mean_errors_per_word == test_case[4]
+            assert self.real_word_error_channel.max_errors_per_word == test_case[5]
+
 
     def test_pass_sentence_through_channel(self):
         r = random.Random(999)
@@ -70,7 +79,7 @@ class RealWordErrorChannelTest(unittest.TestCase):
             [11, 12, 14, 15, 22, 23, 26, 27, 29, 30, 32, 33, 35, 36, 41, 42, 50, 51, 55, 56, 62, 63, 73, 74, 80, 81, 83, 84, 88], \
             [(84, 4, u'<4-digit-integer>')])
         out_file_obj = StringIO.StringIO()
-        self.real_word_error_channel.pass_sentence_through_channel(sentence_and_token_information, random_seed=r)
+        self.real_word_error_channel.pass_sentence_through_channel(sentence_and_token_information, random_number_generator=r)
         expected_text_output = u'xxxxxxxxxxxx'
         assert isinstance(out_file_obj.getvalue(), str), (type(out_file_obj.getvalue()), repr(out_file_obj.getvalue()))
         try:
@@ -93,7 +102,7 @@ class RealWordErrorChannelTest(unittest.TestCase):
         expected_mean_errors_per_word = 1000
         expected_max_errors_per_word = 1000
 
-        error_rate, mean_errors_per_word, max_errors_per_word = self.real_word_error_channel.pass_file_through_channel(text=text_to_create_errors_in, random_seed=r)
+        error_rate, mean_errors_per_word, max_errors_per_word = self.real_word_error_channel.pass_file_through_channel(text=text_to_create_errors_in, random_number_generator=r)
         assert error_rate == expected_error_rate, "Expected error_rate " + str(expected_error_rate) + ", but got " + str(error_rate)
         assert mean_errors_per_word == expected_mean_errors_per_word, "Expected mean_errors_per_word " + str(expected_mean_errors_per_word) + ", but got " + str(mean_errors_per_word)
         assert max_errors_per_word == expected_max_errors_per_word, "Expected max_errors_per_word-rate " + str(expected_max_errors_per_word) + ", but got " + str(max_errors_per_word)
