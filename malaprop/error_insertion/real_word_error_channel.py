0636b3bd885a84ba1a7b8ea08d9401754f5d6845
Amber on abhyasa <amber@abhyasa>
2013-10-18 12:03:36 -0600

    Beginnings of replacement error channel code.

---
+++ malaprop/error_insertion/real_word_error_channel.py
@@ -0,0 +1,82 @@
+# real_word_error_channel.py
+
+import codecs, unicodedata, random, sys
+from copy import deepcopy
+
+class ErrorProbabilities():
+
+    """
+    """
+
+    def __init__ (self, none, substitution, insertion, transposition, deletion):
+
+        assert abs(1.0 - (none + substitution + insertion + transposition + deletion) < 0.000001)
+        self.none = none
+        self.substitution = substitution
+        self.insertion = insertion
+        self.transposition = transposition
+        self.deletion = deletion
+
+class SimpleDamerauLevenshteinChannel():
+
+    """
+    Holds up to one char in a queue, and processes incoming chars.
+    """
+
+    def __init__(self, random_number_generator, probabilities, symbol_set, output_method):
+        
+        self.random_number_generator = random_number_generator
+        self.probabilities = probabilities
+        self.symbol_set = symbol_set
+        self.write = output_method
+        self.queue = []
+
+    def pop(self):
+
+        try:
+            result = self.queue.pop()
+        except:
+            result = None
+        return result
+
+    def push(self, char):
+
+        result = self.pop()
+        self.queue.append(char)
+        assert len(self.queue) < 2, self.queue
+        return result
+
+    def get_substitute(self, char):
+
+        char_index = self.symbol_set.index(char)
+        return self.random_number_generator.choice(self.symbol_set[:char_index]+self.symbol_set[char_index+1:])
+
+    def get_insert(self):
+
+        return self.random_number_generator.choice(self.symbol_set)
+
+    def accept_char(self, char):
+
+        random_number = self.random_number_generator.random()
+
+        if random_number < self.probabilities.none:
+            self.write(self.push(char))
+
+        elif random_number < self.probabilities.none + self.probabilities.substitution:
+            self.write(self.push(self.get_substitute(char)))
+
+        elif random_number < self.probabilities.none + self.probabilities.substitution + self.probabilities.insertion:
+            self.write(self.push(self.get_insert()))
+            self.write(self.push(char))
+
+        elif random_number < self.probabilities.none + self.probabilities.substitution + self.probabilities.insertion + self.probabilities.deletion:
+            self.write(self.pop()) # Dequeue in case of pending transpositions.
+            
+        else: # transposition
+            pending = self.pop()
+            self.push(char)
+            self.write(self.push(pending))
+
+    def flush(self):
+
+        self.write(self.pop())
---
+++ malaprop/test/test_real_word_error_channel.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# test_real_word_error_channel.py
+
+from malaprop.error_insertion import real_word_error_channel
+import unittest, StringIO
+
+def gen(l):
+    for x in l: yield x
+
+class MockRNG:
+    def __init__(self, r_generator, c_generator):
+        self.r_generator = r_generator
+        self.c_generator = c_generator
+    def random(self):
+        return self.r_generator.next() 
+    def choice(self, l):
+        return l[self.c_generator.next()]
+
+class SimpleDamerauLevenshteinChannelTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.error_probabilities = real_word_error_channel.ErrorProbabilities(.2,.2,.2,.2,.2)
+
+    def test_no_error(self):
+
+        output_string = StringIO.StringIO()
+        sdlc = real_word_error_channel.SimpleDamerauLevenshteinChannel(MockRNG(gen([.1]), gen([])), self.error_probabilities, 'abc', output_string.write)
+        sdlc.accept_char('a')
+        sdlc.flush()
+        self.assertEqual(output_string.getvalue(), 'a'), output_string.getvalue()
+
+    def test_substitution(self):
+
+        output_string = StringIO.StringIO()
+        sdlc = real_word_error_channel.SimpleDamerauLevenshteinChannel(MockRNG(gen([.3]), gen([1])), self.error_probabilities, 'abc', output_string.write)
+        sdlc.accept_char('a')
+        sdlc.flush()
+        self.assertEqual(output_string.getvalue(), 'c'), output_string.getvalue()
+
+    def test_insertion(self):
+
+        output_string = StringIO.StringIO()
+        sdlc = real_word_error_channel.SimpleDamerauLevenshteinChannel(MockRNG(gen([.5,.5]), gen([1,2])), self.error_probabilities, 'abc', output_string.write)
+        sdlc.accept_char(None)
+        sdlc.flush()
+        self.assertEqual(output_string.getvalue(), 'b'), output_string.getvalue()
+        sdlc.accept_char('a')
+        sdlc.flush()
+        self.assertEqual(output_string.getvalue(), 'bca'), output_string.getvalue()
+
+    def test_deletion(self):
+
+        output_string = StringIO.StringIO()
+        sdlc = real_word_error_channel.SimpleDamerauLevenshteinChannel(MockRNG(gen([.7]), gen([])), self.error_probabilities, 'abc', output_string.write)
+        sdlc.accept_char('a')
+        sdlc.flush()
+        self.assertEqual(output_string.getvalue(), ''), output_string.getvalue()
+
+    def test_transposition(self):
+
+        output_string = StringIO.StringIO()
+        sdlc = real_word_error_channel.SimpleDamerauLevenshteinChannel(MockRNG(gen([.9, .1,.9]), gen([])), self.error_probabilities, 'abc', output_string.write)
+        sdlc.accept_char('a')
+        sdlc.accept_char('b')
+        sdlc.accept_char('c')
+        sdlc.flush()
+        self.assertEqual(output_string.getvalue(), 'acb'), output_string.getvalue()
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
