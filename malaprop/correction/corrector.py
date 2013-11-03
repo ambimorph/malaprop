fdc8328524cb04cd95ad68b78ac436cd4aa54659
Amber on abhyasa <amber@abhyasa>
2013-11-02 23:05:10 -0600

    Corrector uses the segmenter-tokeniser, BackOffTrigramModel, DamerauLevenshteinDerivor, and HMM to find the most probable combination of variations of a sentence.

---
+++ malaprop/correction/corrector.py
@@ -0,0 +1,73 @@
+# corrector.py
+
+from malaprop.correction.HMM import *
+from recluse.nltk_based_segmenter_tokeniser import *
+
+def match_case(token_to_emulate, lowered_token):
+
+    if token_to_emulate.istitle(): return lowered_token.title()
+    if token_to_emulate.isupper(): return lowered_token.upper()
+    return lowered_token
+
+class Corrector():
+
+    def __init__(self, segmenter_tokeniser, confusion_set_function, backoff_trigram_model_pipe, error_rate):
+
+        self.segmenter_tokeniser = segmenter_tokeniser
+        self.hmm = HMM(confusion_set_function, self.trigram_probability, error_rate)
+        self.backoff_trigram_model_pipe = backoff_trigram_model_pipe
+
+    def trigram_probability(self, three_words):
+
+        """
+        This is a little hack to use for now with BackOffTrigramModel
+        and SRILM trigram models.
+
+        The issue is that SRILM uses only one BOS and EOS marker for
+        every level of language model.  The client is then expected to
+        reduce the order of the query at the sentnce boundaries.  For
+        example, using a trigram model to estimate the probability of
+        the sentence "<s> It was . </s>", we use P(It|<s>) * P(was|<s>
+        It) * P(.|It was) * P(</s>|.).
+
+        However, BackOffTrigramModel does not currently support bigram
+        queries.  Until it does, we will substitute "<last_token> </s>
+        </s>" with "</s> last_token </s>".  Since nothing ever appears
+        after </s>, this will always give us the second bigram.
+        """
+
+        if three_words[1] == '</s>':
+            bigram_inducer = ('</s>', three_words[0], three_words[1])
+            return self.backoff_trigram_model_pipe.trigram_probability(bigram_inducer)
+        else:
+            return self.backoff_trigram_model_pipe.trigram_probability(three_words)
+
+    def correct(self, sentence):
+
+        subtoken_list = [subtokenise(t, abbreviation_list=self.segmenter_tokeniser.sbd._params.abbrev_types) for t in sentence.split()]
+
+        flat_subtokens = [subtoken for token in subtoken_list for subtoken in token]
+        subtoken_count = len(flat_subtokens)
+
+        flat_regular_subtokens = [regularise(subtoken.lower()) for subtoken in flat_subtokens]
+        regularised_indices = [i for i in range(subtoken_count) if flat_regular_subtokens[i]!=flat_subtokens[i].lower()]
+
+        flat_regular_correction = self.hmm.viterbi(flat_regular_subtokens, verbose=False)
+
+        flat_correction = []
+        for i in range(subtoken_count):
+            if i in regularised_indices:
+                flat_correction.append(flat_subtokens[i])
+            else:
+                flat_correction.append(match_case(flat_subtokens[i], flat_regular_correction[i]))
+            
+        corrected_words = (c for c in flat_correction)
+        structured_correction = []
+        for t in subtoken_list:
+            structured_correction.append([])
+            for s in t:
+                structured_correction[-1].append(corrected_words.next())
+
+        return u' '.join([u''.join(subtoken) for subtoken in structured_correction])
+        
+        
---
+++ malaprop/test/test_corrector.py
@@ -0,0 +1,54 @@
+# test_corrector.py
+
+from malaprop.correction.corrector import *
+from recluse.nltk_based_segmenter_tokeniser import *
+from malaprop.correction.HMM import *
+from DamerauLevenshteinDerivor.cderivor import Derivor
+from BackOffTrigramModel.BackOffTrigramModelPipe import BackOffTMPipe
+import unittest, StringIO, subprocess
+
+class MatchCaseTest(unittest.TestCase):
+
+    def test_match_case(self):
+
+        result = match_case('This', 'that')
+        self.assertEqual(result, 'That'), result
+
+        result = match_case('this', 'that')
+        self.assertEqual(result, 'that'), result
+
+        result = match_case('THIS', 'that')
+        self.assertEqual(result, 'THAT'), result
+
+class CorrectorTest(unittest.TestCase):
+
+    def setUp(self):
+        training_text_file = open('malaprop/test/data/segmenter_training', 'r')
+        segmenter_tokeniser = NLTKBasedSegmenterTokeniser(training_text_file)
+        path_to_botmp = subprocess.check_output(['which', 'BackOffTrigramModelPipe']).strip()
+        arpa_file_name = 'malaprop/test/data/trigram_model_2K.arpa'
+        botmp = BackOffTMPipe(path_to_botmp, arpa_file_name)
+        error_rate = 0.3
+        d = Derivor('malaprop/test/data/1K_test_real_word_vocab')
+        self.c = Corrector(segmenter_tokeniser, d.variations, botmp, error_rate)
+
+    def test_correct(self):
+
+        # Regression tests: these results are consistent with the
+        # probabilities of their input, but their passing is not a
+        # guarantee of correctness.
+
+        sentence  = 'It is therefore a more specific from of the term reflectivity.'
+        result = self.c.correct(sentence)
+        expected_result = 'It is therefore a more specific form of the team reflectivity.'
+        self.assertEqual(result, expected_result), result
+
+        sentence  = 'Most land areas are in in albedo range of 0.1 to 0.4.'
+        result = self.c.correct(sentence)
+        expected_result = 'Most land area are win in albedo range of 0.1 to 0.4.'
+        self.assertEqual(result, expected_result), result
+
+
+if __name__ == '__main__':
+    unittest.main()
+
