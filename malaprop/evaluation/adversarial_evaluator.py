675ad5923c8c2d7d42cc83adc51375339b10a97a
Amber on abhyasa <amber@abhyasa>
2013-11-06 11:31:43 -0700

    Adversarial evaluator includes list of missed choices.

--- malaprop/evaluation/adversarial_evaluator.py
+++ malaprop/evaluation/adversarial_evaluator.py
@@ -8,3 +8,22 @@ def report_accuracy(key_file, proposed_file):
     assert len(key_line) == len(proposed_line)
 
     return 1.0 * sum([x==y for (x,y) in zip(key_line,proposed_line)]) / len(key_line)
+
+def report_accuracy_and_errors(key_file, proposed_file, adversarial_file):
+
+    key_line = key_file.readline()
+    proposed_line = proposed_file.readline()
+
+    assert len(key_line) == len(proposed_line)
+
+    pairs = zip(key_line, proposed_line)
+    correct = 0.0
+    error_lines = []
+    for pair in pairs:
+        line = adversarial_file.readline()
+        if pair[0] == pair[1]: correct +=1
+        else: error_lines.append(line)
+
+    return correct / len(key_line), error_lines
+        
+    
--- malaprop/test/test_adversarial_evaluator.py
+++ malaprop/test/test_adversarial_evaluator.py
@@ -15,6 +15,19 @@ class AdversarialEvaluatorTest(unittest.TestCase):
         expected_result = 0.27272727
         self.assertAlmostEqual(result, expected_result), result
 
+    def test_report_accuracy_and_errors(self):
+
+        key_file = StringIO.StringIO('00000000')
+        proposed_file = StringIO.StringIO('01010101')
+        adversarial_file = StringIO.StringIO('a\nb\nc\nd\ne\nf\ng\nh\n')
+
+        acc, errs = report_accuracy_and_errors(key_file, proposed_file, adversarial_file)
+        expected_accuracy = 0.5
+        expected_errs =  ['b\n', 'd\n', 'f\n', 'h\n', ]
+        self.assertAlmostEqual(acc, expected_accuracy), acc
+        self.assertListEqual(errs, expected_errs), errs
+
+
 if __name__ == '__main__':
     unittest.main()
 
