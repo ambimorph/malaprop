19f2681fbe5150ac74917ea8483c9cf44b699368
Amber on abhyasa <amber@abhyasa>
2013-11-06 08:09:17 -0700

    Temporary measure: real words must be ascii-decodable for the derivor to handle them.

--- malaprop/error_insertion/real_word_vocabulary_extractor.py
+++ malaprop/error_insertion/real_word_vocabulary_extractor.py
@@ -15,24 +15,35 @@ class RealWordVocabExtractor():
         letters, and if there are symbols in it, they have to be
         either apostrophes or periods.  In addition, it cannot be the
         special end.of.document word in Westbury Lab's Wikipedia.
+
+        There is also a temporary ban on words containing unicode
+        characters, while I figure out how to get
+        DamerauLevenshteinDerivor, which is written in C to handle
+        unicode.
         """
         for line in self.unicode_vocabfile_obj.readlines():
             word = line.strip()
 
             if word == u'end.of.document':
                 real_word = False
-
+                
             else:
+                try:
+                    word.decode('ascii')
+                except UnicodeEncodeError:
+                    real_word = False
+
+                else:
             
-                contains_letters = False
-                real_word = True
-                for char in word:
-                    if unicodedata.category(char)[0] == 'L':
-                        contains_letters = True
-                    elif char != u'.' and char != u"'":
-                        real_word = False
-                        break
-                if not contains_letters: real_word = False
+                    contains_letters = False
+                    real_word = True
+                    for char in word:
+                        if unicodedata.category(char)[0] == 'L':
+                            contains_letters = True
+                        elif char != u'.' and char != u"'":
+                            real_word = False
+                            break
+                    if not contains_letters: real_word = False
 
             if real_word:
                 self.unicode_outfile_obj.write(word + u'\n')
--- malaprop/test/test_real_word_vocabulary_extractor.py
+++ malaprop/test/test_real_word_vocabulary_extractor.py
@@ -19,6 +19,18 @@ class RealWordVocabExtractorTest(unittest.TestCase):
         for test_word in [u'xxxx', u'-', u'.', u'<3-digit-integer>', u'end.of.document']:
             assert vocabulary.find(u'\n' + test_word + u'\n') == -1, test_word
 
+    def test_no_unicode(self):
+        vocab_file_obj = StringIO.StringIO('Some\nascii\nwords\nand\nthen\n\xe1\xbc\x84\xce\xbd\xce\xb1\xcf\x81\xcf\x87\xce\xbf\xcf\x82\n')
+        outfile_obj = StringIO.StringIO()
+        rwve = real_word_vocabulary_extractor.RealWordVocabExtractor(vocab_file_obj, outfile_obj)
+        rwve.extract_real_words()
+        vocabulary = outfile_obj.getvalue()
+        for test_word in [u'ascii', u'and', u'words']:
+            assert vocabulary.find(u'\n' + test_word + u'\n') != -1, test_word
+        for test_word in [u'\xe1\xbc\x84\xce\xbd\xce\xb1\xcf\x81\xcf\x87\xce\xbf\xcf\x82']:
+            assert vocabulary.find(u'\n' + test_word + u'\n') == -1, test_word
+        
+
         
 if __name__ == '__main__':
     unittest.main()
